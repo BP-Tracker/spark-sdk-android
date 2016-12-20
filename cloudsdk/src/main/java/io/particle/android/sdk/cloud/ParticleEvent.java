72c8087d4732f529f329fccba5394afbfaa309f8
Derek Benda <sleepytonic@gmail.com>
2016-12-20 17:09:45 -0500

    implement Parcelable

--- cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleEvent.java
+++ cloudsdk/src/main/java/io/particle/android/sdk/cloud/ParticleEvent.java
@@ -1,7 +1,12 @@
 package io.particle.android.sdk.cloud;
 
+import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -12,7 +17,37 @@ import javax.annotation.ParametersAreNonnullByDefault;
 // network API changes, then we can write new classes for the network API models, without
 // impacting the public API of the SDK.
 @ParametersAreNonnullByDefault
-public class ParticleEvent {
+public class ParticleEvent implements Parcelable {
+
+    public static final Parcelable.Creator<ParticleEvent> CREATOR = new Parcelable.Creator<ParticleEvent>() {
+        public ParticleEvent createFromParcel(Parcel in) {
+            return new ParticleEvent(in);
+        }
+
+        public ParticleEvent[] newArray(int size) {
+            return new ParticleEvent[size];
+        }
+    };
+
+    protected ParticleEvent(Parcel in) {
+        deviceId = in.readString();
+        dataPayload = in.readString();
+        publishedAt = new Date(in.readLong());
+        timeToLive = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(deviceId);
+        out.writeString(dataPayload);
+        out.writeLong(publishedAt.getTime());
+        out.writeInt(timeToLive);
+    }
 
     @SerializedName("coreid")
     public final String deviceId;
